a101349c80ae0112ffc7dcc575d844e2c531825f
Pedro Valerio <pedrocavalerio0@gmail.com>
2025-10-30 17:49:37 -0300

    Iniciando react no projeto.

---
+++ bionesis/src/app/page.js
@@ -0,0 +1,16 @@
+import "./globals.css";
+import './layout';
+import './page.css';
+
+export default function Home() {
+  return (
+    <>
+      <div className="parent">
+        <div className="container-centralizado">
+        <input type='text' className="caixatexto" placeholder='@teste'>
+        </input>
+      </div>
+      </div>
+    </>
+  );
+}
